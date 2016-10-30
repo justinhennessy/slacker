a083d02814d6e0e3447d9ab95aca82041e124694
Justin Hennessy <justin@neto.com.au>
2016-10-30 11:35:55 +1000

    Changed logging to STDOUT

--- lib/sendlog.rb
+++ lib/sendlog.rb
@@ -3,7 +3,7 @@ require 'logger'
 class SendLog
   def self.log
     if @logger.nil?
-      @logger = Logger.new "./application.log"
+      @logger = Logger.new(STDOUT)
       @logger.level = Logger::DEBUG
       @logger.datetime_format = '%Y-%m-%d %H:%M:%S '
     end
