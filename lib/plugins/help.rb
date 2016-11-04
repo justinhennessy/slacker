e9d2acffc0aeca3538432c2f19240415d369ca85
Justin Hennessy <justin@neto.com.au>
2016-11-04 15:57:19 +1000

    Fixed text output

--- lib/plugins/help.rb
+++ lib/plugins/help.rb
@@ -4,8 +4,6 @@ module Slacker
   module Plugins
     class HelpPlugin < Plugin
       def ready(robot)
-        SendLog.log.info "Loading help plugin ..."
-
         robot.respond /(help)$/i do |message|
           message << help(message.text)
         end
@@ -27,6 +25,8 @@ module Slacker
           "Available commands:\n"
           "@holly show events - currently shows any upcoming events for ec2 instances for staging" \
           "@holly free trials - shows the number of free trials in rackspace production" \
+
+        output << "```"
 
         output
       end
