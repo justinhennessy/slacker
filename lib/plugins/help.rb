8efed4641af54243433e36acf6477749e4de7ffa
Justin Hennessy <justin@neto.com.au>
2016-11-04 16:01:09 +1000

    Fixed text again

--- lib/plugins/help.rb
+++ lib/plugins/help.rb
@@ -22,8 +22,8 @@ module Slacker
         output = "```"
 
         output << \
-          "Available commands:\n" \
-          "@holly show events - currently shows any upcoming events for ec2 instances for staging" \
+          "Available commands:\n\n" \
+          "@holly show events - currently shows any upcoming events for ec2 instances for staging\n" \
           "@holly free trials - shows the number of free trials in rackspace production"
 
         output << "```"
