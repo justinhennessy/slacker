01a89d95ae7fbda1475904b3dfda0cc825d58b1c
Justin Hennessy <justin@neto.com.au>
2016-11-14 20:55:52 +1000

    Added show tainted instances method

--- lib/plugins/aws.rb
+++ lib/plugins/aws.rb
@@ -4,27 +4,61 @@ module Slacker
   module Plugins
     class AWSPlugin < Plugin
       def ready(robot)
-        SendLog.log.info "Inside AWS ready ..."
-        robot.respond /(show events)/i do |message|
+        robot.respond /(show|events|tainted)/i do |message|
           message << aws(message.text)
         end
       end
 
       def aws(text)
-        SendLog.log.info "inside aws"
         #action, build, project, build_number = text.split(" ")
 
-        case text
-        when "show events"
+        case
+        when text.match(/show events/)
           result = show_events
+        when text.match(/show tainted/)
+          result = show_tainted
+        else
+          result = cant_find_response
         end
 
         result
       end
 
       def show_events
-        SendLog.log.info "Inside show_events"
         output_events
+      end
+
+      def show_tainted
+          ec2_client = Aws::EC2::Client.new
+          instances = ec2_client.describe_instances(filters:[{ name: 'tag:tainted', values: ["true"] }])
+
+          tainted_instances_detail = []
+
+          instances.reservations.each { |reservation|
+            reservation.instances.each { |instance|
+              tainted_instances = {}
+              tainted_instances[:instance_id] = instance.instance_id
+              instance.tags.each { |tag|
+                tainted_instances[tag.key] = tag.value if ['Name', 'environment'].include?(tag.key)
+              }
+              tainted_instances_detail.push(tainted_instances)
+            }
+          }
+
+        if tainted_instances_detail.empty?
+          output = "There are no tainted instances"
+        else
+          sorted_instances = tainted_instances_details.sort_by { |k| k["environment"] }
+
+          output = "```"
+
+           sorted_instances.each { |instance|
+             output << "#{instance.name} (#{instance.instance_id}) - #{instance.environment}"
+           }
+
+          output = "```"
+        end
+        output
       end
 
       private
@@ -50,7 +84,7 @@ module Slacker
 
       def instances_with_events
         ec2_client = Aws::EC2::Client.new
-        instances_status = ec2_client.describe_instance_status
+        instances_status = ec2_client.describe_instances(filters:[{ name: 'tag:tainted', values: ["true"] }])
 
         instances_status.instance_statuses.select { |instance|
           !instance.events.empty?
